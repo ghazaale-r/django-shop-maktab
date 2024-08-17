386744b835c3e6f8d1245deef4361e9ba86b6384
ghazaale <qazale02@gmail.com>
2024-08-17 23:55:21 +0330

    feat: Add unit tests for customer model and related functionalities
    Added unit tests for the customer model, including tests for customer creation, profile creation, address relationships, and custom manager functionalities.

---
+++ src/accounts/tests/test_customer.py
@@ -0,0 +1,99 @@
+from django.test import TestCase
+from accounts.models.custom_user import Profile, User
+from accounts.models.customer import Customer, CustomerAddress, Address
+from django.db.utils import IntegrityError
+
+# CustomerManager Tests
+class CustomerManagerTests(TestCase):
+
+    def test_create_customer(self):
+        email = "customer@example.com"
+        password = "password123"
+        customer = Customer.objects.create_customer(email=email, password=password)
+
+        self.assertEqual(customer.email, email)
+        self.assertTrue(customer.check_password(password))
+        self.assertFalse(customer.is_staff)
+        self.assertFalse(customer.is_superuser)
+        self.assertTrue(customer.is_customer)
+
+    def test_create_customer_no_email(self):
+        with self.assertRaises(ValueError):
+            Customer.objects.create_customer(email="", password="password123")
+            
+
+# Customer Tests
+class CustomerTests(TestCase):
+
+    def test_customer_str(self):
+        email = "customer@example.com"
+        customer = Customer.objects.create_customer(email=email, password="password123")
+        self.assertEqual(str(customer), email)
+
+    def test_save_customer(self):
+        email = "customer@example.com"
+        customer = Customer.objects.create_customer(email=email, password="password123")
+
+        self.assertFalse(customer.is_staff)
+        self.assertFalse(customer.is_superuser)
+        self.assertTrue(customer.is_customer)
+
+    def test_get_customer_by_email(self):
+        email = "customer@example.com"
+        Customer.objects.create_customer(email=email, password="password123")
+        
+        customer = Customer.get_customer_by_email(email)
+        self.assertIsNotNone(customer)
+        self.assertEqual(customer.email, email)
+
+        non_existent_customer = Customer.get_customer_by_email("nonexistent@example.com")
+        self.assertFalse(non_existent_customer)
+
+
+# CustomerAddress Tests
+class CustomerAddressTests(TestCase):
+
+    def test_customer_address_str(self):
+        customer = Customer.objects.create_customer(email="customer@example.com", password="password123")
+        address = Address.objects.create(street="123 Main St", city="Tehran", state="Tehran", zipcode="1234567890")
+        customer_address = CustomerAddress.objects.create(customer=customer, address=address, main_address=True)
+
+        self.assertEqual(str(customer_address), f"{customer}-address")
+
+    def test_customer_address_creation(self):
+        customer = Customer.objects.create_customer(email="customer@example.com", password="password123")
+        address = Address.objects.create(street="123 Main St", city="Tehran", state="Tehran", zipcode="1234567890")
+        customer_address = CustomerAddress.objects.create(customer=customer, address=address, main_address=True)
+
+        self.assertTrue(customer_address.main_address)
+        self.assertEqual(customer_address.customer, customer)
+        self.assertEqual(customer_address.address, address)
+
+# Address Tests
+class AddressTests(TestCase):
+
+    def test_address_str(self):
+        address = Address.objects.create(street="123 Main St", city="Tehran", state="Tehran", zipcode="1234567890")
+        self.assertEqual(str(address), "Tehran, Tehran, 123 Main St")
+
+    def test_address_creation(self):
+        address = Address.objects.create(street="123 Main St", city="Tehran", state="Tehran", zipcode="1234567890")
+        self.assertEqual(address.street, "123 Main St")
+        self.assertEqual(address.city, "Tehran")
+        self.assertEqual(address.state, "Tehran")
+        self.assertEqual(address.zipcode, "1234567890")
+        
+
+class CustomerProfileTests(TestCase):
+
+    def test_profile_creation_after_customer_creation(self):
+        email = "customer@example.com"
+        customer = Customer.objects.create_customer(email=email, password="password123")
+
+        self.assertTrue(Profile.objects.filter(user=customer).exists())
+
+        profile = Profile.objects.get(user=customer)
+        self.assertEqual(profile.user, customer) 
+        self.assertEqual(profile.get_fullname(), "کاربر جدید")
+        
+        
